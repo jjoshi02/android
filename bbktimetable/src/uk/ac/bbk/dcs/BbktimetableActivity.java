54c67805f1bd987f69c72901192bab510e6c7e2f
Jay Joshi <jjoshi02@dcs.bbk.ac.uk>
2012-08-07 22:13:46 +0100

    bound uname,pword, button variables

--- bbktimetable/src/uk/ac/bbk/dcs/BbktimetableActivity.java
+++ bbktimetable/src/uk/ac/bbk/dcs/BbktimetableActivity.java
@@ -2,12 +2,34 @@ package uk.ac.bbk.dcs;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class BbktimetableActivity extends Activity {
+public class BbktimetableActivity extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
+	
+	EditText uname;
+	EditText pword;
+	Button submit;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    
+    uname = (EditText)findViewById(R.id.username);
+    pword = (EditText)findViewById(R.id.password);
+    submit = (Button)findViewById(R.id.buttonsubmit);
+    
+    submit.setOnClickListener(this);
+    
     }
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		uname.append("username");
+	}
 }
