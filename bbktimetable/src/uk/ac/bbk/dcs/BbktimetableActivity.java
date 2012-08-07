6f23437166f4d7a06feb3124d2fe245baa51d55b
Jay Joshi <jjoshi02@dcs.bbk.ac.uk>
2012-08-07 16:00:52 +0100

    first commit

---
+++ bbktimetable/src/uk/ac/bbk/dcs/BbktimetableActivity.java
@@ -0,0 +1,13 @@
+package uk.ac.bbk.dcs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BbktimetableActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
