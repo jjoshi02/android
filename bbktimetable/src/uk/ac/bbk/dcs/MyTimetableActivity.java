792191d9f2f632f74530c0470039c53cc434ec66
jjoshi02 <jjoshi02@dcs.bbk.ac.uk>
2012-09-02 18:19:32 +0100

    showing timetable activity on clicking submit

---
+++ bbktimetable/src/uk/ac/bbk/dcs/MyTimetableActivity.java
@@ -0,0 +1,12 @@
+package uk.ac.bbk.dcs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyTimetableActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.timetable);
+	}
+}
