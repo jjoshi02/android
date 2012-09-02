9d33e8ffd73accf5e453d1bf347c3ec905d3ffa8
jjoshi02 <jjoshi02@dcs.bbk.ac.uk>
2012-09-02 22:13:37 +0100

    more refactoring

--- bbktimetable/src/uk/ac/bbk/dcs/MyTimetableActivity.java
+++ bbktimetable/src/uk/ac/bbk/dcs/MyTimetableActivity.java
@@ -7,6 +7,6 @@ public class MyTimetableActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.timetable);
+		setContentView(R.layout.main);
 	}
 }
