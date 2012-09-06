6bc5cfc3a783172ae6f22d1b3389b87a5adae861
jjoshi02 <jjoshi02@dcs.bbk.ac.uk>
2012-09-06 22:27:30 +0100

    shown timetable and on destroy implemented

--- bbktimetable/src/uk/ac/bbk/dcs/MyTimetableActivity.java
+++ bbktimetable/src/uk/ac/bbk/dcs/MyTimetableActivity.java
@@ -1,12 +1,45 @@
 package uk.ac.bbk.dcs;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.net.URLEncoder;
 
-public class MyTimetableActivity extends Activity {
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.webkit.WebView;
+import android.widget.Button;
+
+public class MyTimetableActivity extends Activity implements OnClickListener {
+	private Button buttonquit;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.timetable);
+
+		buttonquit = (Button) findViewById(R.id.buttonquit);
+		WebView webview = (WebView) findViewById(R.id.webview);
+		webview.setBackgroundColor(Color.parseColor("#882233"));
+		String summary = "<html><body>" + BbktimetableActivity.timetableString
+				+ "</body></html>";
+		System.out.println(summary);
+
+		webview.loadData(URLEncoder.encode(summary).replaceAll("\\+", " "),
+				"text/html", "utf-8");
+		buttonquit.setOnClickListener(MyTimetableActivity.this);
+	}
+
+	public void onDestroy() {
+		super.onDestroy();
+	}
+
+	public void onClick(View v) {
+		if (v == buttonquit) {
+			super.onDestroy();
+			System.gc();
+			System.runFinalizersOnExit(true);
+			System.exit(0);
+		}
 	}
 }
